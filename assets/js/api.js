f4755d2e0b15303837e2459a2c463fbe2f929d81
Vinicius-HT <vinicius.h.trindade@gmail.com>
2022-10-18 19:56:22 -0300

    Início

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,6 +1,6 @@
 
 async function fetchProfileData() {
-    const url = 'https://raw.githubusercontent.com/Vinicius-HT/Portifolio/master/data/profile.json';
+    const url = 'https://raw.githubusercontent.com/Vinicius-HT/Portfolio/master/data/profile.json';
     const fetching = await fetch(url)
     return await fetching.json()
 }
